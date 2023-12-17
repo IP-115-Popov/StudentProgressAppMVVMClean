ca818c09984e8c8714b3607864425d91bde08564
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-12-17 18:22:51 +0700

    Implemented closeStudentEditPanelUseCase

--- app/src/main/java/com/sergey/studentprogressappmvvmclean/domain/usecase/CloseStudentEditPanelUseCase.kt
+++ app/src/main/java/com/sergey/studentprogressappmvvmclean/domain/usecase/CloseStudentEditPanelUseCase.kt
@@ -1,4 +1,10 @@
 package com.sergey.studentprogressappmvvmclean.domain.usecase
 
+import android.view.View
+
 class CloseStudentEditPanelUseCase {
+    fun exectute(view: View)
+    {
+        view.visibility = View.GONE
+    }
 }
--- app/src/main/java/com/sergey/studentprogressappmvvmclean/presentation/MainActivity.kt
+++ app/src/main/java/com/sergey/studentprogressappmvvmclean/presentation/MainActivity.kt
@@ -2,11 +2,41 @@ package com.sergey.studentprogressappmvvmclean.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sergey.studentprogressappmvvmclean.R
+import com.sergey.studentprogressappmvvmclean.databinding.ActivityMainBinding
+import com.sergey.studentprogressappmvvmclean.domain.usecase.CloseStudentEditPanelUseCase
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+        get() = _binding ?: throw IllegalStateException("Binding for ActivityMain must not be null")
+    private val closeStudentEditPanelUseCase = CloseStudentEditPanelUseCase()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val llAddStudentPonel: LinearLayout = binding.llAddStudentPonel
+
+        val bthAdd = binding.bthAdd
+        val bthCloseAddStudentPonel = binding.bthCloseAddStudentPonel
+        val bthDownload = binding.bthDownload
+        val bthSave = binding.bthSave
+        bthAdd.setOnClickListener {
+
+        }
+        bthCloseAddStudentPonel.setOnClickListener {
+            closeStudentEditPanelUseCase.exectute(llAddStudentPonel)
+        }
+        bthDownload.setOnClickListener {
+
+        }
+        bthSave.setOnClickListener {
+
+        }
     }
 }
